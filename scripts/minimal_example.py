d4aac2f9ee39e68fb1a7d4025676df7ee3f9974b
Tao Xiang <ge25pof@mytum.de>
2022-09-18 00:09:41 +0200

    test1

--- scripts/minimal_example.py
+++ scripts/minimal_example.py
@@ -1,18 +1,22 @@
 from khl import Bot, Message
 from utils import read_config
-# init Bot
-testBot_token = read_config()['testBot']
-
-bot = Bot(token=testBot_token)
-
-
-# register command, send `/hello` in channel to invoke
-@bot.command(name='hello')
-async def world(msg: Message):
-    await msg.reply('world!')
+import sys
+if __name__ == '__main__':
+    print(sys.path)
 
 
-# everything done, go ahead now!
-bot.run()
-# now invite the bot to a server, and send '/hello' in any channel
-# (remember to grant the bot with read & send permissions)
+# # init Bot
+# testBot_token = read_config()['testBot']
+# bot = Bot(token=testBot_token)
+#
+#
+# # register command, send `/hello` in channel to invoke
+# @bot.command(name='hello')
+# async def world(msg: Message):
+#     await msg.reply('world!')
+#
+#
+# # everything done, go ahead now!
+# bot.run()
+# # now invite the bot to a server, and send '/hello' in any channel
+# # (remember to grant the bot with read & send permissions)

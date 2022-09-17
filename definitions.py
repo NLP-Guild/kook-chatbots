0c5a3fa992f70f3e665045f15c2e1ebd7c4fa0ef
Tao Xiang <ge25pof@mytum.de>
2022-09-18 00:18:16 +0200

    1

--- definitions.py
+++ definitions.py
@@ -1,8 +1,10 @@
 from pathlib import Path
-
+import sys
 def get_project_root() -> Path:
     return Path(__file__).parent
 
 
-# if __name__ == '__main__':
-#     print(get_project_root())
+if __name__ == '__main__':
+    root = str(get_project_root())
+    print(f'adding root path to system path: {{root}}')
+    sys.path.append(root)

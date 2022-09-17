4fb5a4832bf35abd64aecb07d80d277033b72931
Tao Xiang <ge25pof@mytum.de>
2022-09-18 01:38:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-import kook_chatbots
+from khl import Bot, Message
 from kook_chatbots.utils import read_config
 
-print(read_config())
+
+# init Bot
+testBot_token = read_config()['testBot']
+bot = Bot(token=testBot_token)
+
+
+# register command, send `/hello` in channel to invoke
+@bot.command(name='hello')
+async def world(msg: Message):
+    await msg.reply('world!')
+
+
+# everything done, go ahead now!
+bot.run()
+# now invite the bot to a server, and send '/hello' in any channel
+# (remember to grant the bot with read & send permissions)

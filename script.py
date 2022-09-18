034bb2b6e8b25504dbc10f6755fe3d4b3eaa558c
Tao Xiang <ge25pof@mytum.de>
2022-09-18 02:59:31 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from khl import Bot, Message
 from kook_chatbots.utils import read_config
 from kook_chatbots.api_ import print_msg_info
-
+import asyncio
 # init Bot
 testBot_token = read_config()['testBot']
 bot = Bot(token=testBot_token)
@@ -10,8 +10,10 @@ bot = Bot(token=testBot_token)
 # register command, send `/hello` in channel to invoke
 @bot.command(name='hello')
 async def world(msg: Message):
-    await print_msg_info(msg)
+    task1 = asyncio.create_task(print_msg_info(msg))
+    # await print_msg_info(msg)
     await msg.reply('world!')
+    await task1
 
 
 
